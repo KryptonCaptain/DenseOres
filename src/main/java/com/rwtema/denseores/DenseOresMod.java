8740e725026e7b6843c04491ff68829bb380e9ec
KryptonCaptain <cjalcorn.s446@gmail.com>
2016-09-04 22:24:43 +1000

    unimportant updoot
    Doubled checked load order for Strata compat
    Incremented version by 1

--- src/main/java/com/rwtema/denseores/DenseOresMod.java
+++ src/main/java/com/rwtema/denseores/DenseOresMod.java
@@ -11,7 +11,9 @@ import net.minecraftforge.common.MinecraftForge;
 
 import java.io.File;
 
-@Mod(modid = DenseOresMod.MODID, version = DenseOresMod.VERSION, dependencies = "after:*")
+@Mod(modid = DenseOresMod.MODID, version = DenseOresMod.VERSION, 
+dependencies = 	"after:*")
+
 public class DenseOresMod {
     public static final String MODID = "denseores";
     public static final String VERSION = "1.0";
